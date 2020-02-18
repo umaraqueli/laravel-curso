5e088e9a2294a3a145ca96be2d9d7ab57d05b9bc
umaraqueli <raqueliggom@gmail.com>
2020-02-18 20:41:09 -0300

    Prepopulate Users and questions using Model Factories and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,14 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run(){
+  
+        factory(App\User::class, 3)->create()->each(function($u) {
+            $u->questions()
+                ->saveMany(
+                factory(App\Question::class, rand(1, 5))->make()
+                );
+        });  
     }
 }
+
